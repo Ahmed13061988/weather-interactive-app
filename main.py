89e7ea99b2e301851dbe1bea81476ab34a0dbbba
Ahmed Hussein <wolktheline@yahoo.com>
2024-08-31 19:40:57 -0600

    Building a graph user interface using streamlit.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+st.title("Weather forcast for next days")
+
+place = st.text_input("Place: ")
+
+days = st.slider("Forecast days", min_value=1, max_value=5, help="""
+    The number of forecasted days 
+""")
+
+
+data = st.selectbox("Select data to view", options=["Temperature", "Sky"])
+
+
+st.subheader(f"{data} for the next {days} days in {place}")
