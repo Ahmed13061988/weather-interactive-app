d940aed913ab4e34940d2360686126309c81be10
Ahmed Hussein <wolktheline@yahoo.com>
2024-10-08 21:51:00 -0600

    Adding the data dynamically from the api. Adding the images of the sky condition.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ st.title("Weather forcast for next days")
 
 place = st.text_input("Place: ")
 
+
 days = st.slider("Forecast days", min_value=1, max_value=5, help="""
     The number of forecasted days 
 """)
@@ -17,8 +18,10 @@ st.subheader(f"{option} for the next {days} days in {place}")
 if place:
     filtered_content = get_data(place, days)
 
+
+
     if option == "Temperature":
-        temp = [i["main"]["temp"] for i in filtered_content]
+        temp = [i["main"]["temp"] / 10 for i in filtered_content]
 
         dates = [i["dt_txt"] for i in filtered_content]
 
