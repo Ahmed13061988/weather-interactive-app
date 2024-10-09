d7e8a0c10b2a89f8dc3df82b7b28bc27b0f025ad
Ahmed Hussein <wolktheline@yahoo.com>
2024-10-08 21:05:50 -0600

    Using a real API to get the weather data for specific city for many days we need.

--- backend.py
+++ backend.py
@@ -7,9 +7,16 @@ def get_data(place, days, option):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     content = response.json()
-    return content
+    filtered_content = content["list"]
+    filtered_content = filtered_content[:8 * days]
+    if option == "Temperature":
+        filtered_content = [i["main"]["temp"] for i in filtered_content]
+    elif option == "Sky":
+        filtered_content = [i["weather"][0]["main"] for i in filtered_content]
+
+    return filtered_content
 
 
 if __name__ == "__main__":
-    data = get_data("Tokyo", None, None)
+    data = get_data("Tokyo", 3, "Sky")
     print(data)
